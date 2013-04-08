dd4b47ae894441b52565d7ba3a1f0d0d2dc0fe82
Wade Stebbings <wade@umn.edu>
2013-04-07 20:48:25 -0500

    somewhat functional abiltity to get flikr photos and render them in the page

--- public/js/images-model.js
+++ public/js/images-model.js
@@ -1,11 +1,45 @@
 
+/*
+    s	small square 75x75
+    q	large square 150x150
+    t	thumbnail, 100 on longest side
+
+    m	small, 240 on longest side
+    n	small, 320 on longest side
+
+    -	medium, 500 on longest side
+
+    z	medium 640, 640 on longest side
+    c	medium 800, 800 on longest side†
+    b	large, 1024 on longest side*
+    o	original image, either a jpg, gif or png, depending on source format
+*/
+
 var ImageModel = Backbone.Model.extend({
+
   initialize: function() {
-    this.set('image_id', '654321');
-    this.set('proxy_url', '/proxy?url=' + this.get('url_o'));
-    console.log("Image Model:");
+    console.log('img model');
     console.log(this);
+    this.set('image_id', 'i' + this.get('id'));
+    var url = this.find_first(['url_l', 'url_o', 'url_b', 'url_c', 'url_z']);
+    if (url) {
+      this.set('proxy_url', '/proxy?url=' + url);
+    } else {
+      console.log("GOING NOWHERE FAST!");
+    }
+  },
+
+  find_first: function(list) {
+    var self = this;
+    _.each(list, function(item) {
+      var candidate = self.get(item);
+      if (candidate) {
+        return candidate;
+      }
+    });
+    return false;
   }
+
 });
 
 
@@ -13,46 +47,27 @@ var ImageDataSource = Backbone.Collection.extend({
   model: ImageModel,
 
   initialize: function(options) {
-    var self = this;
-    _.bindAll(this, 'flickr_success');
-    self.whoami = 'ImageDataSource'; //debugging
-    // TODO: setup Flickr stuff here
+    _.bindAll(this, 'flickr_search');
+    this.whoami = 'ImageDataSource'; //debugging
   },
 
-  // Public API called from CombinedView:
-  //   request_add
-  //   request_remove
-  //   request_update
+  // "Public API" - used from CombinedView:
+  //    request_add
+  //    request_remove
+  //    request_update
 
-  // request_add is really 'enqueue'
-  //   .. since Flickr will take a bit of time. Then, the callback
-  //   will do the real add and create the ImageModel instance.
   request_add: function(id, latitude, longitude, magnitude, depth) {
     // presently, magnitude & depth are ignored/not used.
-    console.log('ImageDataSource :: request_add');
+    //console.log('ImageDataSource :: request_add');
     this.flickr_search({
       lon: longitude,
       lat: latitude,
       radius: 32
-    }, this.flickr_success);
+    });
   },
 
-  flickr_success: function(i) {
-    console.log("Flickr SUCCESS");
-    console.log( i );
-  },
-
-  // flickr_search({
-  //   text: 'search text of some type',
-  //   lon: longitude
-  //   lat: latitude
-  //   radius: 32
-  // }, callback_function);
-  flickr_search: function(params, success) {
+  flickr_search: function(params) {
         var self = this;
-
-        success = success || $.noop;
-
         var base_params = {
           api_key : cfg.key,
           sort : 'interestingness-desc',
@@ -63,11 +78,7 @@ var ImageDataSource = Backbone.Collection.extend({
           page : 1,
           license : ''
         };
-
         var data_params = _.extend(base_params, params);
-        console.log('data_params');
-        console.log(data_params);
-
         $.ajax({
             url : cfg.flickr_url,
             data : data_params,
@@ -75,16 +86,9 @@ var ImageDataSource = Backbone.Collection.extend({
             jsonp : 'jsoncallback',
             success : function (response) {
                 var photo_list = response.photos.photo;
-                //console.log('photo_list');
-                //console.log(photo_list);
-                console.log('->self');
-                console.log(self);
-
-                //success(photo_list);
                 self.add(photo_list);
             }
         });
-
   },
 
   test: function() {
@@ -97,15 +101,15 @@ var ImageDataSource = Backbone.Collection.extend({
     self.add(o);
   },
 
-  request_remove: function(id) {
-    console.log('ImageDataSource :: request_remove');
+  request_remove: function(image_id) {
+    //console.log('ImageDataSource :: request_remove');
     var self = this;
-    var model = self.get(eq_event_id);
+    var model = self.get(image_id);
     self.remove(model);
   },
 
-  request_update: function(id) {
-    console.log('ImageDataSource :: request_update');
+  request_update: function(image_id) {
+    //console.log('ImageDataSource :: request_update');
     //TBD
   }
 
