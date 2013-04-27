36c2e2c19c24f670d91a1f615a61aadc7cbe7acb
Wade Stebbings <wade@umn.edu>
2013-04-27 16:30:24 -0500

    render canvas by iterating through list of hidden images

--- public/js/view.js
+++ public/js/view.js
@@ -34,47 +34,72 @@ var ImagesView = Backbone.View.extend({
     canvas.height = viewport.height;
   },
 
-  render_to_1canvas: function(im, hscale, alpha) {
+  render_canvas: function() {
     var self = this;
     var canvas = document.getElementById("c1"); // not really the backbone idiom
     var ctx = canvas.getContext('2d');
-    // alpha (0-1): conversion from opacity (0-100 percent)
-    ctx.globalAlpha = alpha;
-    // horizontal scaling
-    ctx.scale(hscale, 1.0);
-    ctx.drawImage(im,0,0, viewport.width, viewport.height);
-  },
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  render_canvas: function(img, hstretch, opacity) {
-    var self = this;
+    var images = $('#hidden-images img');
+    _.each(images, function( im ) {
+      var jqimg = $(im);
+      var depth = jqimg.data("depth");
+      var mag = jqimg.data("mag");
+      var search = jqimg.data("search");
 
-    var alpha = (opacity / 100.0);
-    self.render_to_1canvas(img, hstretch, alpha);
+      // depth translates to blur, from 0-800 range to 0.0-3.0
+      var blur_by_depth = (depth / 200.0);
+      // mag(nitude) translates to opacity & horizontal-stretch
+      //   ranges:: mag: 0-10, opacity: 10-90 percent, hstretch: 1.0-3.0
+      var opacity_by_mag = Math.floor(8.0 * mag + 10.0); // percent: 10-90 range
+      var hstretch_by_mag = 1.1; // (mag / 5.0) + 1.0;
+      var hscale = hstretch_by_mag;
+
+      var alpha = (opacity_by_mag / 100.0);
+      if (search === 'secondary') {
+        alpha = alpha * 0.25;
+      }
+
+      Pixastic.process(im, "blurfast", {amount: blur_by_depth});
+
+      // alpha (0-1): conversion from opacity (0-100 percent)
+      ctx.globalAlpha = alpha;
+      ctx.scale(hscale, 1.0); // horizontal scaling
+      ctx.drawImage(im,0,0, viewport.width, viewport.height);
+    });
   },
 
   render_image: function(img_element, img_model) {
-    var self = this;
+    var search = img_model.get('search');
+
+/*
+    var msg = "==> RENDER_IMAGE (to hidden area): " + img_element.id;
+    if (search === 'primary') {
+      msg = msg + " .. via PRIMARY search!!";
+    } else if (search === 'secondary') {
+      msg = msg + " .. via SECONDARY search";
+    } else {
+      msg = msg + " .. via ways unknown -- is that not amazing?";
+    }
+    console.log(msg);
+*/
+
+    $('#hidden-images').append(img_element); // storage place for images
+
     var depth = img_model.get('depth');
     var mag = img_model.get('mag');
-    // depth translates to blur, from 0-800 range to 0.0-3.0
-    var blur_by_depth = (depth / 200.0);
-    // mag(nitude) translates to opacity & horizontal-stretch
-    //   ranges:: mag: 0-10, opacity: 10-90 percent, hstretch: 1.0-3.0
-    //var opacity_by_mag = (parseFloat(img_model.get('mag')) / 12.0);
-    var opacity_by_mag = Math.floor(8.0 * mag + 10.0); // percent: 10-90 range
-    var hstretch_by_mag = (mag / 5.0) + 1.0;
 
-    // image size/width & opacity
+    // set image size/width & opacity & data-attributes
     var jq_img = $(img_element);
     var vp_width = viewport.width;
     if (vp_width > 50) {
       jq_img.attr({width: vp_width});
     }
-    jq_img.css({opacity: opacity_by_mag});
-
-    Pixastic.process(img_element, "blurfast", {amount: blur_by_depth});
-
-    self.render_canvas(img_element, hstretch_by_mag, opacity_by_mag);
+    jq_img.attr({
+      'data-depth': depth,
+      'data-mag': mag,
+      'data-search': search
+    });
   },
 
 
@@ -86,12 +111,11 @@ var ImagesView = Backbone.View.extend({
     var iid = img_model.get('image_id'); // flickr image id
     var eid = img_model.get('eqid');
     console.log("onAdd(image) iid=" + iid + " for " + eid);
-
     var img = new Image();
     img.onload = function(ev) {
       var img_elem = ev.srcElement;
-      $('#hidden-images').append(img_elem); // storage place for images
       self.render_image(img_elem, img_model);
+      self.render_canvas();
     }
     img.id = eid;
     img.src = url;
